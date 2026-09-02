fcfa794f59324f8d907b642c2d21d9488e9c96b9
Onur Esin <onuresn034@gmail.com>
2026-09-02 12:55:48 +0300

    feat: setup Tailwind CSS v4

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,12 @@ function App() {
     <>
       <section id="center">
         <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={heroImg} className="base" width="170" height="179" alt=""  />
           <img src={reactLogo} className="framework" alt="React logo" />
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+          <h1 >Get started</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
